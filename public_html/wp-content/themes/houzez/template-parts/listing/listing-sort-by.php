e15cbe753834857a0ad73928fe0eb1cad25c4c1f
nguyen.nh <nguyen.nh@tinasoft.vn>
2023-08-01 03:07:49 +0700

    Change text

--- public_html/wp-content/themes/houzez/template-parts/listing/listing-sort-by.php
+++ public_html/wp-content/themes/houzez/template-parts/listing/listing-sort-by.php
@@ -20,15 +20,15 @@ if(houzez_is_half_map()) {
 		<div class="sort-by-title">
 			<?php esc_html_e( 'Sắp xếp theo:', 'houzez' ); ?>
 		</div><!-- sort-by-title -->  
-		<select id="<?php echo esc_attr($sort_id); ?>" class="selectpicker form-control bs-select-hidden" title="<?php esc_html_e( 'Default Order', 'houzez' ); ?>" data-live-search="false" data-dropdown-align-right="auto">
-			<option value=""><?php esc_html_e( 'Default Order', 'houzez' ); ?></option>
-			<option <?php selected($sortby, 'a_price'); ?> value="a_price"><?php esc_html_e('Price - Low to High', 'houzez'); ?></option>
-            <option <?php selected($sortby, 'd_price'); ?> value="d_price"><?php esc_html_e('Price - High to Low', 'houzez'); ?></option>
+		<select id="<?php echo esc_attr($sort_id); ?>" class="selectpicker form-control bs-select-hidden" title="<?php esc_html_e( 'Mặc định', 'houzez' ); ?>" data-live-search="false" data-dropdown-align-right="auto">
+			<option value=""><?php esc_html_e( 'Mặc định', 'houzez' ); ?></option>
+			<option <?php selected($sortby, 'a_price'); ?> value="a_price"><?php esc_html_e('Giá - Thấp tới cao', 'houzez'); ?></option>
+            <option <?php selected($sortby, 'd_price'); ?> value="d_price"><?php esc_html_e('Giá - Cao tới thấp', 'houzez'); ?></option>
             
-            <option <?php selected($sortby, 'featured_first'); ?> value="featured_first"><?php esc_html_e('Featured Listings First', 'houzez'); ?></option>
+            <option <?php selected($sortby, 'featured_first'); ?> value="featured_first"><?php esc_html_e('Danh sách nổi bật đầu tiên', 'houzez'); ?></option>
             
-            <option <?php selected($sortby, 'a_date'); ?> value="a_date"><?php esc_html_e('Date - Old to New', 'houzez' ); ?></option>
-            <option <?php selected($sortby, 'd_date'); ?> value="d_date"><?php esc_html_e('Date - New to Old', 'houzez' ); ?></option>
+            <option <?php selected($sortby, 'a_date'); ?> value="a_date"><?php esc_html_e('Ngày - Cũ đến Mới', 'houzez' ); ?></option>
+            <option <?php selected($sortby, 'd_date'); ?> value="d_date"><?php esc_html_e('Ngày - Mới đến Cũ', 'houzez' ); ?></option>
 		</select><!-- selectpicker -->
 	</div><!-- d-flex -->
 </div><!-- sort-by -->
